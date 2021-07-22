f1ed0d006e2ee740075c3e2658cf9881828b01a8
Levon <levon.hambardzumyan02@gmail.com>
2021-07-23 01:53:38 +0400

    second changes

--- contracts/flipaCoin.sol
+++ contracts/flipaCoin.sol
@@ -2,21 +2,24 @@
 import "hardhat/console.sol";
 pragma solidity ^0.8.6;
 contract FlipACoin{
-    int side;
-    constructor (){
-        
+    uint index;
+    constructor (uint _index){
+        index = _index;
     }
-    function confirm() public pure returns(int){
-         return 0;
-    }
-    
-    function play(string memory _betAmount, int _symbol)public view{
-        int a = confirm();
-        if (a == _symbol) {
-            console.log("You win ", _betAmount);   
-        } else {
-            console.log("You lose", _betAmount);
+    function confirm() private view returns (uint) {
+        return block.timestamp % 2;
+} 
+    function play(uint _symbol)public payable{
+        uint a = confirm();
+        //require(address(this).balance > (msg.value * index)/100);
+        if(a == _symbol){
+            payable(msg.sender).transfer(msg.value*(100+index)/100);   
+            console.log("You win", _symbol);
         }
+        else{
+            console.log("You lose", _symbol);
+        }
+
     }
     
 
--- test/flipcoin.js
+++ test/flipcoin.js
@@ -4,11 +4,19 @@ const { expect } = require("chai");
 describe("Test1", function(){
     it("Send right message", async function(){
         const Flip = await ethers.getContractFactory("FlipACoin") 
-        const flip = await Flip.deploy();
+        const flip = await Flip.deploy(90);
         await flip.deployed();
         
-        await flip.play("1000", 0);
-        await flip.play("1000", 1);
+        await flip.play(0);
+        await flip.play(0);
+        await flip.play(0);
+        await flip.play(0);
+        await flip.play(0);
+        await flip.play(0);
+        await flip.play(0);
+        await flip.play(0);
+        await flip.play(0);
+        await flip.play(0);
     });
 
 });
